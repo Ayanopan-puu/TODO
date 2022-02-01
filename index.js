20b4d7275e4c47e034c35c021237a6467ab1f78b
Ayanopan-puu <bkwkin.m15@gmail.com>
2022-02-01 14:38:04 +0900

    追加（TODOを見える化・ulを活用/liの中点を消す）

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-//コメント
-console.log("Hello!");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
+
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log();
+  add();
+});
+
+function add() {
+  const li = document.createElement("li");
+  li.innerText = input.value;
+  li.classList.add("list-group-item");
+  ul.appendChild(li);
+  li.style.listStyle = "none";
+  input.value = "";
+}
