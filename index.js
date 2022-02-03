7a0fd59ecdc48836f04bb3c0d03ca1ef833965e6
Ayanopan-puu <bkwkin.m15@gmail.com>
2022-02-03 15:12:51 +0900

    追加（空白の場合TODOに入れない）

--- index.js
+++ index.js
@@ -5,15 +5,28 @@ const ul = document.getElementById("ul");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log();
   add();
 });
 
 function add() {
-  const li = document.createElement("li");
-  li.innerText = input.value;
-  li.classList.add("list-group-item");
-  ul.appendChild(li);
-  li.style.listStyle = "none";
-  input.value = "";
+  let todoText = input.value;
+  if(todoText.length > 0) {
+    const li = document.createElement("li");
+    li.innerText = todoText;
+    li.classList.add("list-group-item");
+    ul.appendChild(li);
+    li.style.listStyle = "none";
+    input.value = "";
+    saveData();
+  }
+}
+
+
+function saveData() {
+  const lists = document.querySelectorAll("li");
+  let todos = [];
+  lists.forEach(list => {
+    todos.push(list.innerText);
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
