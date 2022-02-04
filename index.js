803dbe55cf8b1056414bf418c2748059bfdf40d9
Ayanopan-puu <bkwkin.m15@gmail.com>
2022-02-04 17:01:57 +0900

    ストレージに残す

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
 
+const todos JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach(todo => {
+    add(todo);
+  } )
+}
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   add();
 });
 
-function add() {
+function add(todo) {
   let todoText = input.value;
+
+  if (todo) {
+    todoText = todo;
+  }
   if(todoText.length > 0) {
     const li = document.createElement("li");
     li.innerText = todoText;
