92b92c4f44cc6eea1275cd7f7cec418b63d2290d
Ayanopan-puu <bkwkin.m15@gmail.com>
2022-02-04 21:40:29 +0900

    あらかた完成

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
 
-const todos JSON.parse(localStorage.getItem("todos"));
+const todos = JSON.parse(localStorage.getItem("todos"));
 
 if (todos) {
   todos.forEach(todo => {
@@ -19,12 +19,28 @@ function add(todo) {
   let todoText = input.value;
 
   if (todo) {
-    todoText = todo;
+    todoText = todo.text;
   }
   if(todoText.length > 0) {
     const li = document.createElement("li");
     li.innerText = todoText;
     li.classList.add("list-group-item");
+
+    if (todo && todo.completed) {
+      li.classList.add("text-decoration-line-through");
+    }
+
+    li.addEventListener("contextmenu", function (event) {
+      event.preventDefault();
+      li.remove();
+      saveData();
+    });
+
+    li.addEventListener("click", function () {
+      li.classList.toggle("text-decoration-line-through");
+      saveData();
+    });
+
     ul.appendChild(li);
     li.style.listStyle = "none";
     input.value = "";
@@ -36,8 +52,13 @@ function add(todo) {
 function saveData() {
   const lists = document.querySelectorAll("li");
   let todos = [];
+
   lists.forEach(list => {
-    todos.push(list.innerText);
+    let todo = {
+      text: list.innerText,
+      completed: list.classList.contains("text-decoration-line-through")
+    };
+    todos.push(todo);
   });
   localStorage.setItem("todos", JSON.stringify(todos));
 }
